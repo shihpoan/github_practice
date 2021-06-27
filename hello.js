fc80fdca842c1a55e40719793337e63cc2a20cbc
shihpoan <randypunk123@gmail.com>
2021-06-27 18:10:22 +0800

    npm init, npm i, start

--- hello.js
+++ hello.js
@@ -1,2 +1,14 @@
-console.log("Hello github");
-console.log("gaga");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8888;
+app.get("/", (req, res) => {
+    res.end("Hello World");
+});
+
+app.get("/test", (req, res) => {
+    res.end("You are in the /test");
+});
+
+app.listen(port, () => {
+    console.log(`server listen on port =${port}`)
+});
